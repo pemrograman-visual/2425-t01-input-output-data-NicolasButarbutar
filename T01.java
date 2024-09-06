df7e0700b3218274e2f0e8eef6c3d40d577b2ce5
Nicolas Butarbutar <nicolaszuliaaan@gmail.com>
2024-09-06 11:22:50 +0700

    Sudah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,43 @@
-// NIM - Your Name
+// 12S24038 - NICOLAS J GRACE BUTARBUTAR
+// 12S24026 - GRISELDA TABITHA NATHANIA HUTAHAEAN
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        String judul;
+
+        judul = input.nextLine();
+        String penulis;
+
+        penulis = input.nextLine();
+        int tahun;
+
+        tahun = input.nextInt();
+        String penerbit;
+
+        penerbit = input.nextLine();
+        String format;
+
+        format = input.nextLine();
+        double harga;
+
+        harga = input.nextDouble();
+        double margin;
+
+        margin = input.nextDouble();
+        int stok;
+
+        stok = input.nextInt();
+        double rating;
+
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + format + "|" + harga + "|" + margin + "|" + stok + "|" + rating);
+    }
+}
